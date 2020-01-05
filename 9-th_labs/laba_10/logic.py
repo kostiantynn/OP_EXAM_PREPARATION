e5e83491033a7c1a0a583b7509977651719ff567
Kostiantyn <kostya.code@gmail.com>
2020-01-06 00:35:22 +0200

    Laba_9-10

---
+++ 9-th_labs/laba_10/logic.py
@@ -0,0 +1,20 @@
+def __find_same__(string):
+    arr_index = []
+    new_string = ''.join(list(string))
+    for i in range(len(string)):
+        for j in range(len(string)):
+            if string[i] == string[j] and i != j:
+                arr_index.append(i)
+                arr_index.append(j)
+    return set(arr_index)
+
+
+def delete_same(string):
+    delete_index_arr = __find_same__(string)
+    l_string = list(string)
+    j = 0
+    for i in delete_index_arr:
+        i -= j
+        l_string.pop(i)
+        j+=1
+    return ''.join(l_string)
